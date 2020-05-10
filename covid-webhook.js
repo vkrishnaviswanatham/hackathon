a9588a03155a408cccdf07a322d2c0efde5650cb
vkrishnaviswanatham <vviswanath129@gmail.com>
2020-05-10 17:35:38 +0530

    commit

--- covid-webhook.js
+++ covid-webhook.js
@@ -1,12 +1,4 @@
-/**
- *
- * main() will be run when you invoke this action
- *
- * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
- *
- * @return The output of this action, which must be a JSON object.
- *
- */
+
 var request = require("request-promise");
 const DiscoveryV1 = require("watson-developer-cloud/discovery/v1");
 
